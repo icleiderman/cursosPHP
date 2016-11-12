beee39b06f37a59251fd91c48cc65f237abf46dd
icleiderman <cleiderman@hotmail.com>
2016-11-12 13:12:59 -0600

    upd cesco

--- Facturas/conexion.php
+++ Facturas/conexion.php
@@ -3,8 +3,8 @@
 
 	$MY_HOST = "localhost";
 	$MY_DB_USER = "root";
-	//$MY_DB_PW = "cesco2016";
-	$MY_DB_PW = "12345678";
+	$MY_DB_PW = "cesco2016";
+	//$MY_DB_PW = "12345678";
 	$MY_DB_NAME = "php_curso";
 
 
